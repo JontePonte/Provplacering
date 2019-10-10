2b618fabf85206d335f8004422fbfe848c628594
JontePonte <john_nordstrand@hotmail.com>
2019-10-10 20:31:05 +0200

    Sista småfix

--- Location.py
+++ Location.py
@@ -3,5 +3,5 @@ class Location:
     """ Egenskap för skrivplats """
     def __init__(self, info):
         self.occupied = False
-        self.student_name = "N"
+        self.student_name = "None"
         self.x_cor, self.y_cor = info
